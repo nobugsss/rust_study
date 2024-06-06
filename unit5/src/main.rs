02aa94a0e1996b8eba858e97feb3af02e1c24521
nobugsss <tlxc1224@163.com>
2024-06-06 14:54:23 +0800

    unit5

--- unit5/src/main.rs
+++ unit5/src/main.rs
@@ -22,15 +22,52 @@ fn main() {
 
     // let rect1 = (30, 50);
     // println!("The area of the rectangle is {} square pixels.", area(rect1));
-    
+
+    // let rect1 = Rectangle {
+    //     width: 30,
+    //     height: 50,
+    // };
+    // println!("rect1={:?}", rect1);
+    // println!(
+    //     "The area of the rectangle is {} square pixels.",
+    //     area(&rect1)
+    // );
+    // dbg!(&rect1);
+    // let scale = 2;
+    // let rect2 = Rectangle {
+    //     width: dbg!(30 * scale),
+    //     height: 50,
+    // };
+    // dbg!(&rect2);
+
+    // 方法语法
+    // let rect1 = Rectangle {
+    //     width: 30,
+    //     height: 50,
+    // };
+    // println!(
+    //     "The area of the rectangle is {} square pixels.",
+    //     rect1.area()
+    // )
+
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("rect1={:?}", rect1);
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
-
-
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+    let sq = Rectangle::square(30);
+    println!("sq= {:?}", sq);
+    println!("sq= {:#?}", sq);
+    dbg!(sq);
 }
 
 // struct User {
@@ -60,12 +97,49 @@ fn main() {
 //     dimensios.0 * dimensios.1
 // }
 
- #[derive(Debug)]
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+
+// 方法语法
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+
+#[derive(Debug)]
+
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+impl Rectangle {
+    // fn area(&self) -> u32 {
+    //     self.width * self.height
+    // }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
