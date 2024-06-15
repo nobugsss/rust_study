a179671dff2e8aceb72339ffe307d9c83af23c91
nobugsss <tlxc1224@163.com>
2024-06-16 00:28:28 +0800

    unit16 end

--- unit16/src/main.rs
+++ unit16/src/main.rs
@@ -1,3 +1,171 @@
+use std::rc::Rc;
+use std::sync::mpsc;
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    // thread::spawn(|| {
+    //     for i in 1..10 {
+    //         println!("hi number {i} from the spawned thread!");
+    //         thread::sleep(Duration::from_millis(1));
+    //     }
+    // });
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {i} from the spawned thread!");
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+    let v = vec![1, 2, 3];
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {v:?}");
+    });
+    for i in 1..5 {
+        println!("hi number {i} from the main thread!");
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+    handle2.join().unwrap();
+
+    // 16.2. 使用消息传递在线程间通信
+    // 创建一个信道，并将其两端赋值给 tx 和 rx
+    // let (tx, rx) = mpsc::channel();
+    // thread::spawn(move || {
+    //     let val = String::from("hi");
+    //     tx.send(val).unwrap();
+    //     println!("val is {val}"); //所有权发生转移，这里不能再用val
+    //     for val in vals {
+    //         tx.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+    // let received = rx.recv().unwrap();
+    // println!("Got: {received}");
+
+    // 发送多个值并观察接收者的等待
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("hi"),
+    //         String::from("from"),
+    //         String::from("the"),
+    //         String::from("thread"),
+    //     ];
+
+    //     for val in vals {
+    //         tx.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+    // for received in rx {
+    //     println!("Got: {received}");
+    // }
+
+    // 通过克隆发送者来创建多个生产者
+    // let tx1 = tx.clone();
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("hi"),
+    //         String::from("from"),
+    //         String::from("the"),
+    //         String::from("thread"),
+    //     ];
+
+    //     for val in vals {
+    //         tx1.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+
+    // thread::spawn(move || {
+    //     let vals = vec![
+    //         String::from("more"),
+    //         String::from("messages"),
+    //         String::from("for"),
+    //         String::from("you"),
+    //     ];
+
+    //     for val in vals {
+    //         tx.send(val).unwrap();
+    //         thread::sleep(Duration::from_secs(1));
+    //     }
+    // });
+
+    // for received in rx {
+    //     println!("Got: {received}");
+    // }
+
+    // 使用互斥器
+
+    // let m = Mutex::new(5);
+    // {
+    //     let mut num = m.lock().unwrap();
+    //     *num = 6;
+    // }
+    // println!("m = {m:?}");
+
+    // 在线程间共享 Mutex<T>
+
+    // 此例报错，所有权被移动了
+    // let counter = Mutex::new(0);
+    // let mut handles = vec![];
+
+    // for _ in 0..10 {
+    //     let handle = thread::spawn(move || {
+    //         let mut num = counter.lock().unwrap();
+
+    //         *num += 1;
+    //     });
+    //     handles.push(handle);
+    // }
+
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
+
+    // println!("Result: {}", *counter.lock().unwrap());
+
+    // 多线程和多所有权。使用 Rc<T> 来允许多个线程拥有 Mutex<T>
+    // Rc<T> 并不能安全的在线程间共享。当 Rc<T> 管理引用计数时，它必须在每一个 clone 调用时增加计数，并在每一个克隆被丢弃时减少计数。
+    // Rc<T> 并没有使用任何并发原语，来确保改变计数的操作不会被其他线程打断。
+    // 在计数出错时可能会导致诡异的 bug，比如可能会造成内存泄漏，或在使用结束之前就丢弃一个值
+    // let counter = Rc::new(Mutex::new(0));
+    // let mut handles = vec![];
+
+    // for _ in 0..10 {
+    //     let counter = Rc::clone(&counter);
+    //     let handle = thread::spawn(move || {
+    //         let mut num = counter.lock().unwrap();
+
+    //         *num += 1;
+    //     });
+    //     handles.push(handle);
+    // }
+
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
+
+    // println!("Result: {}", *counter.lock().unwrap());
+
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
 }
