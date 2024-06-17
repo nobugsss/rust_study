009fa9d46385769a46dc711f87e18655d4368693
nobugsss <tlxc1224@163.com>
2024-06-17 21:54:45 +0800

    unit18 end

--- unit18/src/main.rs
+++ unit18/src/main.rs
@@ -1,3 +1,219 @@
 fn main() {
-    println!("Hello, world!");
+    let favorite_color: Option<&str> = None;
+    let is_tuesday = false;
+    let age: Result<u8, _> = "34".parse();
+
+    if let Some(color) = favorite_color {
+        println!("Using your favorite color, {color}, as the background");
+    } else if is_tuesday {
+        println!("Tuesday is green day!");
+    } else if let Ok(age) = age {
+        if age > 30 {
+            println!("Using purple as the background color");
+        } else {
+            println!("Using orange as the background color");
+        }
+    } else {
+        println!("Using blue as the background color");
+    }
+    // println!("age:{:?}", age);
+
+    let mut stack = Vec::new();
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    while let Some(top) = stack.pop() {
+        println!("top:{}", top);
+    }
+
+    let v = vec![1, 2, 3, 4];
+    for (index, value) in v.iter().enumerate() {
+        println!("index={index},value={value}")
+    }
+
+    let x = Some(5);
+    let y = 10;
+    println!("x = {x:?}");
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {y}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
+
+    let x = 1;
+
+    match x {
+        1 | 2 => println!("one or two"),
+        3 => println!("three"),
+        _ => println!("anything"),
+    }
+
+    let x = 5;
+
+    match x {
+        1..=5 => println!("one through five"),
+        _ => println!("something else"),
+    }
+
+    let x = 'c';
+
+    match x {
+        'a'..='j' => println!("early ASCII letter"),
+        'k'..='z' => println!("late ASCII letter"),
+        _ => println!("something else"),
+    }
+
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    let p = Point { x: 0, y: 7 };
+
+    match p {
+        Point { x, y: 0 } => println!("On the x axis at {x}"),
+        Point { x: 0, y } => println!("On the y axis at {y}"),
+        Point { x, y } => {
+            println!("On neither axis: ({x}, {y})");
+        }
+    }
+
+    // 解构枚举
+    // let msg = Message::ChangeColor(0, 160, 255);
+
+    // match msg {
+    //     Message::Quit => {
+    //         println!("The Quit variant has no data to destructure.");
+    //     }
+    //     Message::Move { x, y } => {
+    //         println!("Move in the x direction {x} and in the y direction {y}");
+    //     }
+    //     Message::Write(text) => {
+    //         println!("Text message: {text}");
+    //     }
+    //     Message::ChangeColor(r, g, b) => {
+    //         println!("Change the color to red {r}, green {g}, and blue {b}")
+    //     }
+    // }
+
+    // 解构结构体和元组
+    // let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    foo(3, 4);
+
+    // 使用嵌套的 _ 忽略部分值
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {setting_value:?}");
+
+    // 忽略元组的多个部分
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {first}, {third}, {fifth}")
+        }
+    }
+
+    let s = Some(String::from("Hello!"));
+
+    // if let Some(s) = s {
+    //     println!("found a string");
+    // }
+    match s {
+        Some(ss) => println!("xxxxxs:{}", ss),
+        _ => (),
+    }
+    // println!("{s:?}");
+
+    // 用 .. 忽略剩余值
+    #[derive(Debug)]
+    struct Point2 {
+        x: i32,
+        y: i32,
+        z: i32,
+    }
+
+    let origin = Point2 { x: 0, y: 0, z: 0 };
+
+    match origin {
+        Point2 { x, .. } => println!("x issssss {x}"),
+    }
+    println!("origin: {:?}", origin);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, .., last) => {
+            println!("Some numbers: {first}, {last}");
+        }
+    }
+
+    // 匹配守卫提供的额外条件
+    let num = Some(4);
+
+    match num {
+        Some(x) if x % 2 == 0 => println!("The number {x} is even"),
+        Some(x) => println!("The number {x} is odd"),
+        None => (),
+    }
+
+    // 使用匹配守卫来测试与外部变量的相等性
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {n}"),
+        _ => println!("Default case, x = {x:?}"),
+    }
+
+    println!("at the end: x = {x:?}, y = {y}");
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"),
+        _ => println!("no"),
+    }
+
+    // @ 绑定
+    enum Message {
+        Hello { id: i32 },
+    }
+
+    let msg = Message::Hello { id: 5 };
+
+    match msg {
+        Message::Hello {
+            id: id_variable @ 3..=7,
+        } => println!("Found an id in range: {id_variable}"),
+        Message::Hello { id: 10..=12 } => {
+            println!("Found an id in another range")
+        }
+        Message::Hello { id } => println!("Found some other id: {id}"),
+    }
+}
+
+// enum Message {
+//     Quit,
+//     Move { x: i32, y: i32 },
+//     Write(String),
+//     ChangeColor(i32, i32, i32),
+// }
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {y}");
 }
