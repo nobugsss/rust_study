ed9261ef982f1f033d527f41c8333b3c53c75ef7
nobugsss <tlxc1224@163.com>
2024-06-19 15:38:19 +0800

    find Error after

--- unit19/src/main.rs
+++ unit19/src/main.rs
@@ -1,9 +1,10 @@
-use hello_macro::HelloMacro;
 use hello_macro_derive::HelloMacro;
+use unit19_hello_macro::HelloMacro;
 
 #[derive(HelloMacro)]
 struct Pancakes;
 
 fn main() {
     Pancakes::hello_macro();
+    println!("Hello");
 }
