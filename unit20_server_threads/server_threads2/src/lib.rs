438a37621fd9f338607364c51f29825bba18dfae
nobugsss <tlxc1224@163.com>
2024-06-29 13:28:50 +0800

    update unit20_server_threads

--- unit20_server_threads/server_threads2/src/lib.rs
+++ unit20_server_threads/server_threads2/src/lib.rs
@@ -33,9 +33,23 @@ impl ThreadPool {
     }
 }
 
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            // worker.thread.join().unwrap();
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
+}
+
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    // thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
@@ -48,6 +62,10 @@ impl Worker {
             job();
         });
 
-        Worker { id, thread }
+        Worker {
+            id,
+            // thread
+            thread: Some(thread),
+        }
     }
 }
--- unit20_server_threads/server_threads2/src/main.rs
+++ unit20_server_threads/server_threads2/src/main.rs
@@ -1,3 +1,4 @@
+use server_threads2::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
@@ -5,7 +6,6 @@ use std::{
     thread,
     time::Duration,
 };
-use unit20_server_threads::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
