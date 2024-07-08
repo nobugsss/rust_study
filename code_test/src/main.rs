c59e7bfea230eae7fcf601e3c505e92208e00f71
nobugsss <tlxc1224@163.com>
2024-07-08 11:20:45 +0800

    update code_test

--- code_test/src/main.rs
+++ code_test/src/main.rs
@@ -131,6 +131,9 @@ fn main() {
         // 现在需要 `FnOnce`。
         drop(farewell);
     };
+
+    let v = List2(vec![1, 2, 3]);
+    println!("{}", v);
 }
 
 struct School {
@@ -238,5 +241,30 @@ impl List {
                 format!("Nil")
             }
         }
+    }
+}
+
+// 定义一个包含单个 `Vec` 的结构体 `List`。
+struct List2(Vec<i32>);
+
+impl fmt::Display for List2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // 使用元组的下标获取值，并创建一个 `vec` 的引用。
+        let vec = &self.0;
+
+        write!(f, "[")?;
+
+        // 使用 `v` 对 `vec` 进行迭代，并用 `count` 记录迭代次数。
+        for (count, v) in vec.iter().enumerate() {
+            // 对每个元素（第一个元素除外）加上逗号。
+            // 使用 `?` 或 `try!` 来返回错误。
+            if count != 0 {
+                write!(f, ", ")?;
+            }
+            write!(f, "{}:{}", count, v)?;
+        }
+
+        // 加上配对中括号，并返回一个 fmt::Result 值。
+        write!(f, "]")
     }
 }
